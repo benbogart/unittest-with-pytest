40f1d4a8ee4e335c2481aed65f523c9558f7cde1
Ben Bogart <ben@benbogart.com>
2021-07-16 23:57:32 -0400

    add readme content

--- project.py
+++ project.py
@@ -5,12 +5,12 @@ def add(a, b):
     return a + b
 
 
-def subtract(a, b):
-    return a - b
-
-
 def add_col(df, colname, coldata):
     
     # add a column to a DataFrame
     df[colname] = coldata
     return df
+
+
+def subtract(a, b):
+    return a - b
